040ad0d2d36696ef88cc672c33fbeee0de6bd959
Alessandro Molari <molari.alessandro@gmail.com>
2020-04-12 11:04:33 +0200

    Fix fasd cache dir

--- external-tools/fasd.zsh
+++ external-tools/fasd.zsh
@@ -1,6 +1,11 @@
 if [ $commands[fasd] ]; then
 
   fasd_cache="~/.fasd-cache"
+
+  if [[ ! -d "${fasd_cache}" ]]; then
+    mkdir -p "${fasd_cache}"
+  fi
+
   if [ "$(command -v fasd)" -nt "${fasd_cache}" -o ! -s "${fasd_cache}" ]; then
     fasd --init auto >| "${fasd_cache}"
   fi
