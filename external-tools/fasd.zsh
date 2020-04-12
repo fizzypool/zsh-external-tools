bc4cf5a8c08bd7d99289abbbc086939a2248340d
Alessandro Molari <molari.alessandro@gmail.com>
2020-04-12 10:35:39 +0200

    Add fasd

---
+++ external-tools/fasd.zsh
@@ -0,0 +1,12 @@
+if [ $commands[fasd] ]; then
+
+  fasd_cache="${ZSH_CACHE_DIR}/fasd-init-cache"
+  if [ "$(command -v fasd)" -nt "${fasd_cache}" -o ! -s "${fasd_cache}" ]; then
+    fasd --init auto >| "${fasd_cache}"
+  fi
+
+  source "${fasd_cache}"
+
+  unset fasd_cache
+
+fi
