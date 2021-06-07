95dae4780799b48e0fc11f543d6f8a239e8f0c4c
Alessandro Molari <66269365+cl-alessandro-molari@users.noreply.github.com>
2021-06-07 10:36:56 +0200

    Update todo.zsh

--- external-tools/todo.zsh
+++ external-tools/todo.zsh
@@ -1,10 +1,3 @@
-# PyEnv support
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
-fi
-
 # NVM support
 export NVM_DIR="$HOME/.nvm"
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
@@ -27,11 +20,3 @@ export PATH="/Applications/VMware OVF Tool:${PATH}"
 
 # Set pipenv maximum upper directory search for projects.
 export PIPENV_MAX_DEPTH=100
-
-function tracktime() {     
-    start=$(gdate +%s%N)
-    zsh -c $1
-    end=$(gdate +%s%N)
-    runtime=$(((end-start)/1000000000.0))
-    echo "$runtime ms"     
-}
