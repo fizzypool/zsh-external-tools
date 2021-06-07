2088b5391ab2592492a3365c1909e7c88d931419
Alessandro Molari <66269365+cl-alessandro-molari@users.noreply.github.com>
2021-06-07 10:37:10 +0200

    Create pyenv.zsh

---
+++ external-tools/pyenv.zsh
@@ -0,0 +1,27 @@
+if [ $commands[pyenv] ]; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="${PYENV_ROOT}/bin"
+  if [[ $OSTYPE == darwin* ]]; then
+    # Note: Manual init to speedup
+    export PATH=":${PYENV_ROOT}/shims:${PATH}"
+    export PYENV_SHELL=zsh
+    source "/usr/local/Cellar/pyenv/*/completions/pyenv.zsh"
+    command pyenv rehash 2>/dev/null
+    function pyenv() {
+      local command
+      command="${1:-}"
+      if [ "$#" -gt 0 ]; then
+        shift
+      fi
+
+      case "$command" in
+      rehash|shell)
+        eval "$(pyenv "sh-$command" "$@")";;
+      *)
+        command pyenv "$command" "$@";;
+      esac
+    }
+  elif [[ $OSTYPE == linux* ]]; then
+    eval $(pyenv init -)
+  fi
+fi
