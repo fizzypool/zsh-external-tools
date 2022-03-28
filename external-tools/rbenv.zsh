4894c9f33e280cf1d3516715002e4c680e2c6408
Alessandro Molari <66269365+cl-alessandro-molari@users.noreply.github.com>
2022-03-28 13:10:12 +0200

    Create rbenv.zsh

---
+++ external-tools/rbenv.zsh
@@ -0,0 +1,18 @@
+export PATH="/Users/cl-alessandro-molari/.rbenv/shims:${PATH}"
+export RBENV_SHELL=zsh
+source '/opt/homebrew/Cellar/rbenv/1.2.0/libexec/../completions/rbenv.zsh'
+command rbenv rehash 2>/dev/null
+rbenv() {
+  local command
+  command="${1:-}"
+  if [ "$#" -gt 0 ]; then
+    shift
+  fi
+
+  case "$command" in
+  rehash|shell)
+    eval "$(rbenv "sh-$command" "$@")";;
+  *)
+    command rbenv "$command" "$@";;
+  esac
+}
