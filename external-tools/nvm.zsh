d58f772aeeb46ca8e7edff1539b1e478a71f7dfa
Alessandro Molari <66269365+cl-alessandro-molari@users.noreply.github.com>
2022-03-28 13:09:06 +0200

    Update nvm.zsh

--- external-tools/nvm.zsh
+++ external-tools/nvm.zsh
@@ -1,9 +1,17 @@
 if [ -d "$HOME/.nvm" ]; then
   export NVM_DIR="$HOME/.nvm"
+  
+  # Load NVM
   if [ -s "/usr/local/opt/nvm/nvm.sh" ]; then
     source "/usr/local/opt/nvm/nvm.sh"
+  elif [ -s "/opt/homebrew/opt/nvm/nvm.sh" ]; then
+    source "/opt/homebrew/opt/nvm/nvm.sh"
   fi
-  if [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ]; then
+  
+  # Load NVM completion
+  if [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ]; then
+    source "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+  elif [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ]; then
     source "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
   fi
 fi
