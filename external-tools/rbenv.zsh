08ee610e6bc3083d9205cd79af941f43efdd0a8c
Alessandro Molari <66269365+cl-alessandro-molari@users.noreply.github.com>
2022-03-28 13:11:16 +0200

    Update rbenv.zsh

--- external-tools/rbenv.zsh
+++ external-tools/rbenv.zsh
@@ -1,18 +1,27 @@
-export PATH="/Users/cl-alessandro-molari/.rbenv/shims:${PATH}"
-export RBENV_SHELL=zsh
-source '/opt/homebrew/Cellar/rbenv/1.2.0/libexec/../completions/rbenv.zsh'
-command rbenv rehash 2>/dev/null
-rbenv() {
-  local command
-  command="${1:-}"
-  if [ "$#" -gt 0 ]; then
-    shift
-  fi
 
-  case "$command" in
-  rehash|shell)
-    eval "$(rbenv "sh-$command" "$@")";;
-  *)
-    command rbenv "$command" "$@";;
-  esac
-}
+
+if [ $commands[rbenv] ]; then
+  if [[ $OSTYPE == darwin* ]]; then
+    # Note: Manual init to speedup
+    export PATH="${HOME}/.rbenv/shims:${PATH}"
+    export RBENV_SHELL=zsh
+    source '/opt/homebrew/Cellar/rbenv/1.2.0/libexec/../completions/rbenv.zsh'
+    command rbenv rehash 2>/dev/null
+    rbenv() {
+      local command
+      command="${1:-}"
+      if [ "$#" -gt 0 ]; then
+        shift
+      fi
+
+      case "$command" in
+      rehash|shell)
+        eval "$(rbenv "sh-$command" "$@")";;
+      *)
+        command rbenv "$command" "$@";;
+      esac
+    }
+  elif [[ $OSTYPE == linux* ]]; then
+    eval $(pyenv init -)
+  fi
+fi
